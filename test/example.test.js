b5e278c5ca7bb3886c74373213092f87875eb905
carlos <carlos.rueda@itglobers.com>
2024-06-04 15:39:10 -0500

    add testing

---
+++ test/example.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
+    });
+
+    it('should return the index when the value is present', function() {
+      expect([1, 2, 3].indexOf(2)).to.equal(1);
+    });
+  });
+});
